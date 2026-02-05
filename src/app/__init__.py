396157ff95bce06771834ccb51d76ad069eb93eb
Colin Reed <manamanadude@gmail.com>
2026-02-05 11:49:33 -0700

    Initializing pages and format

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,19 +4,18 @@ from flask import Flask
 import os
 
 app = Flask('Hardware Genie')
-# app.secret_key = os.environ['SECRET_KEY']
 app.secret_key = 'you will never know'
 
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
-# db initialization
-#from flask_sqlalchemy import SQLAlchemy
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///parts.db'
-#db = SQLAlchemy(app)
+#db initialization
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///parts.db'
+db = SQLAlchemy(app)
 
 # models initialization
-#from app import models
-#with app.app_context(): 
-#    db.create_all()
+from app import models
+with app.app_context(): 
+    db.create_all()
 
 from app import routes
--- src/app/routes.py
+++ src/app/routes.py
@@ -7,3 +7,23 @@ from app import app
 def index():
     """Render the main index page."""
     return render_template('index.html')
+
+@app.route('/memorygraphs', methods=['GET', 'POST'])
+def memory_graphs():
+    """Render the memory page."""
+    return render_template('memory_graphs.html')
+
+@app.route('/gpugraphs', methods=['GET', 'POST'])
+def gpu_graphs():
+    """Render the GPU page."""
+    return render_template('gpu_graphs.html')
+
+@app.route('/cpugraphs', methods=['GET', 'POST'])
+def cpu_graphs():
+    """Render the CPU page."""
+    return render_template('cpu_graphs.html')
+
+@app.route('/storagegraphs', methods=['GET', 'POST'])
+def storage_graphs():
+    """Render the Storage page."""
+    return render_template('storage_graphs.html')
