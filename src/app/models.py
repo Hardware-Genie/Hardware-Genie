cc2582268d821bac60a0c7daa4e4158505d93e46
Colin Reed <manamanadude@gmail.com>
2026-02-10 10:52:29 -0700

    setting model to example and changing header

--- src/app/models.py
+++ src/app/models.py
@@ -2,17 +2,17 @@ from datetime import date
 from app import db
 
 
-
+# EXAMPLE MODEL
 # listing parts.
-class Part(db.Model):
-    "Class that defines parts"
-    __tablename__ = 'parts'
-    id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
-    name = db.Column(db.String, nullable=False)
-    description = db.Column(db.String)
-    price = db.Column(db.Float, nullable=False)
-    available = db.Column(db.Boolean, default=True)
-    image_url = db.Column(db.String)
-
-    def __str__(self):
-        return f'<Part(id={self.id}, name={self.name}, price={self.price})>'
+#class Part(db.Model):
+#    "Class that defines parts"
+#    __tablename__ = 'parts'
+#    id = db.Column(db.Integer, primary_key=True ,autoincrement=True)
+#    name = db.Column(db.String, nullable=False)
+#   description = db.Column(db.String)
+#    price = db.Column(db.Float, nullable=False)
+#    available = db.Column(db.Boolean, default=True)
+#    image_url = db.Column(db.String)
+#
+#    def __str__(self):
+#        return f'<Part(id={self.id}, name={self.name}, price={self.price})>'
