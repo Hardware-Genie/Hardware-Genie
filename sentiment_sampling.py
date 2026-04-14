652ea83f72f1de77837f7760a7bcaca45abaa651
CesarJSoto <csoto15@msudenver.edu>
2026-04-14 10:47:07 -0600

    Altered sentiment analysis to kind of work. Needs more tuning but should be good till we can make our own model

--- sentiment_sampling.py
+++ sentiment_sampling.py
@@ -1,18 +1,32 @@
-# Use a pipeline as a high-level helper
+import nltk
+
 from transformers import pipeline
 from nltk.tokenize import sent_tokenize
 
+#currentl using distilroberta-finetuned-financial-news-model
 classifier = pipeline("text-classification", model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
 
-#Perform sentiment analysis on sample texts
-with open("test_article") as file:
-    article = file.read()
+def sentemantic_analysis(text):
+    pos = 0
+    neg = 0
+    
+    #Tokenizes the article by blank line. Currently expects just headlines, though full articles have been tested.
+    article_paragraph = sent_tokenize(text) 
+    results = classifier(article_paragraph)
+    
+    for article_paragraph, result in zip(article_paragraph, results):
+        print(f"Text: {article_paragraph}")
+        print(f"Label: {result['label']}, Score: {result['score']:.4f}\n")
 
-article_sentence = sent_tokenize(article)
-
-
-results = classifier(article_sentence)
-
-for article_sentence, result in zip(article_sentence, results):
-    print(f"Text: {article_sentence}")
-    print(f"Label: {result['label']}, Score: {result['score']:.4f}\n")
+        #Keeps track of number of positive or negative sentences to determine overall sentiment
+        if result["label"] == "positive" and result["score"] > .7:
+            pos =+ 1
+        elif result["label"] == "negative" and result["score"] > .7:
+            neg =+ 1
+                
+    if pos > neg:
+        return "positive"
+    elif neg > pos:
+        return "negative"
+    else:
+        return "unsure"
